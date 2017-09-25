f91ce0f01ceb20ef8f912e787ba535471c0433c8
Shawn McKinney <smckinney@apache.org>
2017-09-24 22:06:54 -0500

    add instructions

--- src/main/resources/hacker-script.sh
+++ src/main/resources/hacker-script.sh
@@ -2,7 +2,6 @@
 # no license
 echo "You've been hacked"
 echo 'CLASSPATH='$CLASSPATH
-#touch 'YouveBeenHacked'
 echo "You've been hacked ur CLASSPATH="$CLASSPATH >> YouveBeenHacked
 echo "ur passwords=" >> YouveBeenHacked
 cat '/etc/passwd' > YouveBeenHacked
