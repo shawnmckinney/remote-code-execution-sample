91c7d997cc7ca1f3c1f6462d17ff9ec362f41f98
Shawn McKinney <smckinney@apache.org>
2017-09-25 23:18:34 -0500

    add a few env vars to hackfile

--- src/main/resources/hacker-script.sh
+++ src/main/resources/hacker-script.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 # no license
 echo "You've been hacked"
-echo 'CLASSPATH='$CLASSPATH
-echo "You've been hacked ur CLASSPATH="$CLASSPATH >> YouveBeenHacked
-echo "ur passwords=" >> YouveBeenHacked
-cat '/etc/passwd' > YouveBeenHacked
+echo "CLASSPATH="$CLASSPATH
+echo "You've been hacked!"  > YouveBeenHacked
+echo "USER="$USER >> YouveBeenHacked
+echo "PWD="$PWD >> YouveBeenHacked
+echo "JAVA CLASSPATH="$CLASSPATH >> YouveBeenHacked
+echo "YOUR PASSWORDS=" >> YouveBeenHacked
+cat '/etc/passwd' >> YouveBeenHacked
