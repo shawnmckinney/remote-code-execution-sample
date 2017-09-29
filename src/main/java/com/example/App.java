35dac9182a9dee36068c0688012faab7940d9441
Shawn McKinney <shawnmckinney@users.noreply.github.com>
2017-09-29 08:35:04 -0500

    Add some more details about the exploit.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -24,19 +24,20 @@ public class App
         System.out.println( "Begin serial exploit test...." );
         App myApp = new App();
 
-        // Obviously, out in the wild bad code won't be instantiated like this. Rather, it'll occur during reflection that is triggered within an XML parsing or file upload operation.
+        // This will occurr at some point before the exploit, on the hacker's machine.
         BadCode myObj = new BadCode();
         myObj.name = "duke";
         myObj.address = "moscone center";
         System.out.println("Input: " + myObj.name + " " + myObj.address);
 
-        // 1. The object serialization might happen elsewhere. It serializes the contents of a rogue object to a stream and is the conduit for attack.
+        // 1. The object serialization will also happen on hacker's machine. It serializes the contents of this rogue object to a stream and will be uploaded to host (intended target) via input form or web service.
         myApp.serialize( myObj );
 
-        // 2. In a real-world exploit there'll be some sort of resource activity that occurs here.  It might be an HTTP invocation over the network triggering XML parsing of data in the payload, or maybe files being uploaded to a Web page ( in the case of Equifax).
+        // 2. In a real-world exploit there'll be some sort of resource activity where the serialized object is transmitted to the host.  
+        // It might be an HTTP invocation over the network triggering XML parsing of data in the payload, or maybe files being uploaded to a Web page ( in the case of Equifax).
 
-        // 3. The object deserialization might happen elsewhere in the system, perhaps on another tier, in another datacenter.
-        // It is during this method invocation the exploit gets triggered by this hapless app.
+        // 3. The object deserialization occurrs on the machine being targeted.
+        // It is during this method invocation the exploit gets triggered by the hapless app.
         myObj = myApp.deserialize();
 
         //Print the result:
